41363592d3d3dda7f598b8b9552e811e9d7cbab5
Roberto Albuquerque <95970556+robertooca@users.noreply.github.com>
2022-08-10 17:09:32 -0300

    Update q2.asm

--- Lista1/q2.asm
+++ Lista1/q2.asm
@@ -1,57 +1,54 @@
-addi x9, x0, 0x0d
+addi x9, x0, 0xd
+xor x12, x12, x12
+
 # a (x10)
-addi sp, sp, -36
-jal x14, gets	
-addi x10, x15, 0
-sb x10, 24(sp)
+jal x1, inic
+addi x10, x12, 0
+add x12, x0, x0
+#b (x14)
+jal x1, inic
+addi x14, x12, 0
+add x12, x0, x0
 
-# b (x11)
-jal x14, gets
-addi x11, x15, 0
-sb x11, 16(sp)
+# c (x15)
+jal x1, inic
+addi x15, x12, 0
+add x12, x0, 0
 
-# c (x12)
-jal x14, gets
-addi x12, x15, 0
-sb x12, 8(sp)
+# x (x16)
+addi x16, x0, 0
 
-# x (x13)
-jal x14, gets
-addi x13, x15, 0
-sb x13, 0(sp)
-
-# funcao p/ pegar numero
-gets:
-	lw x15, 1025(x0)
-	beq x15, x9, done
-	slli x16, x15, 3
-	slli x15, x15, 1
-	add x15, x15, x16
-	jal x17, gets
-done:
-	lw x15, 1024(x0)
-	jalr x0, 0(x14)
-# x = 0
-addi x13, x0, 0
 # a >= 0
 bge x10, x0, and_
+jal
 
 # b <= 64 (0040)
 and_:
-	addi x14, x0, 0040
-	bge x14, x11, and__
-	jalr x0, 0(x1)
+	addi x18, x0, 0040
+	bge x18, x14, and__	
+	jal x0, end
 and__:
-	addi x14, x0, 0017
-	blt x14, x12, if
-if:
-	addi x13, x13, 1
-halt
+	addi x18, x0, 0017
+	blt x18, x15, if
+	jal x0, and
 
-# ENUNCIADO DA QUESTAO
-# Encontre a menor sequência de instruções RISC-V que implementa a código abaixo, onde a,b,c e x são variáveis na memória. 
-# Teste um array de entrada com 15 sequencias diferentes de a, b e c, que deverão ser dadas pelo teclado. 
-# Os resultados e as entradas deverão ser visualizadas no display.
-# int a = ...; int b = ...; int c = ...; int x = ...;
-# x = 0;
-# if ( a >= 0 && b <= 64 && c > 23 ) x = 1;
+if:
+	addi x16, x16, 1
+	jal x0, end
+end: halt
+
+# jalr x0,0(x0)
+inic:
+	lb x11, 1025(x0)
+	beq x11, x9, endloop
+	sb x11, 1024(x0)
+	jal x0, gets
+gets:
+	slli x13, x12, 1
+	slli x12, x12, 3
+	add x12, x13, x12
+	addi x11, x11, -48
+	add x12, x12, x11
+	jal x0, inic
+endloop:
+	jalr x0, 0(x1)

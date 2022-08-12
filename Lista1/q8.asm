d7eb31b732155a7d4c1ffc0cc3cc460f3d16370b
Roberto Albuquerque <95970556+robertooca@users.noreply.github.com>
2022-08-12 16:48:57 -0300

    Update q8.asm

--- Lista1/q8.asm
+++ Lista1/q8.asm
@@ -1,12 +1,11 @@
-addi x9, x0, 4
-xor x15, x15, x15
+addi x9, x0, 4 
 
 gets:
-	lb x12, 1025(x0) # teclado
+	slli x14, x14, 1
+	lb x12, 1025(x0)
 	addi x12, x12, -48
 	add x14, x14, x12
-	add x14, x14, x14 # multiplicando por 2
-	addi x13, x13, 1 # flag
+	addi x13, x13, 1
 	beq x9, x13, cmp
 	jal x0, gets
 
@@ -30,46 +29,52 @@ cmp:
 	beq x14, x15, print8
 	addi x15, x15, 1 
 	beq x14, x15, print9
+	addi x15, x15, 6
 	beq x14, x15, limpaDisplay
 
 print0:
-	lh x10, 0b0111111
+	addi x10, x10, 0b0111111
 	jal x0, printDisplay
 print1:
-	addi x10, x0, 0b0000110
+	addi x10, x0, 0b000110
 	jal x0, printDisplay
 print2:
-	addi x10, x0, 0b1011011
+	addi x10, x0, 0b011011
+	addi x16, x0, 0b1
 	jal x0, printDisplay
 print3:
-	addi x10, x0, 0b1001111
+	addi x10, x0, 0b001111
+	addi x16, x0, 0b1
 	jal x0, printDisplay
 print4:
-	addi x10, x0, 0b1100110
+	addi x10, x0, 0b100110
+	addi x16, x0, 0b1
 	jal x0, printDisplay
 print5:
-	addi x10, x0, 0b1101101
+	addi x10, x0, 0b101101
+	addi x16, x0, 0b1
 	jal x0, printDisplay
 print6:
-	addi x10, x0, 0b1111101
+	addi x10, x0, 0b111101
+	addi x16, x0, 0b1
 	jal x0, printDisplay
 print7:
-	addi x10, x0, 0b0000111
+	addi x10, x0, 0b000111
 	jal x0, printDisplay
 print8:
-	addi x10, x0, 0b1111111
+	addi x10, x0, 0b111111
+	addi x16, x0, 0b1
 	jal x0, printDisplay
 print9:
-	addi x10, x0, 0b1101111
+	addi x10, x0, 0b101111
+	addi x16, x0, 0b1
 	jal x0, printDisplay
 limpaDisplay:
-	addi x10, x0, 0b0000001
+	addi x10, x0, 0
 	jal x0, printDisplay
-
+	
 printDisplay:
 	sh x10, 1029(x0)
-	addi x10, x10, 1
-	jal x0, printDisplay
-
-end: halt
+	sh x16, 1027(x0)
+	halt
 

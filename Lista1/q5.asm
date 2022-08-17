2d9cd5629ca733944ad2070339130d8854b56ac8
Roberto Albuquerque <95970556+robertooca@users.noreply.github.com>
2022-08-17 16:47:05 -0300

    Add files via upload

--- Lista1/q5.asm
+++ Lista1/q5.asm
@@ -1,42 +1,73 @@
-ponto1: .string "AEIOU" # 0  # salvando string
-ponto2: .string "DGT" # 4
-ponto3: .string "BCMNP" # 8
-ponto4: .string "FHVWY" # 12
-ponto5: .string "KRS" # 16
-ponto8: .string "JLX" # 20
-ponto10: .string "QZ"   # 24
-
-addi x12, x0, 13 # enter
-# pegando inputs (x10 e x11)
+addi x10, x0, 13 # enter
+addi x14, x0, 0 # contador 1 de pontos
 gets:
-    lw x10, 1025(x0)
-    addi sp, sp, -8
-	sw x10, 0(sp)
-    beq x10, x12, cmp
-	beq x0, x0, gets
+	lb x11, 1025(x0)
+	beq x11, x10, end
+	addi x12, x0, 18
+	addi x13, x0, 0 # contador
+	iterar_string:
+		lb x5, 0(x12)
+		beq x5, x11, cmp
+		addi x12, x12, 1
+		addi x13, x13, 1 # vendo onde ta na str
+	bne x5, x0, iterar_string
+	
+	jal x0, gets
 
-# iterar e comparar pelas strings
 cmp:
-    addi x5, x0, 0 # i
-    lw x13, 0(x0)
-    loop1:
-        # comparando com ponto2
-        lw x14, 4(x0)
-        add x14, x5, x0
-        lb x14, 0(x14)
-        beq x14, x12, count
-        # comparando com ponto3
-        lw x15, 8(x0)
-        # comparando com ponto4
-        lw x16, 12(x0)
-        # comparando com ponto5
-        lw x17, 16(x0)
-        # comparando com ponto8
-        lw x18, 20(x0)
-        # comparando com ponto10
-        lw x19, 24(x0)
-        addi x10, x0, 0
-        # ir p/ cmp2
+	addi x6, x0, 5
+	blt x13, x6, ad1
+	addi x6, x0, 8
+	blt x6, x0, ad2
+	addi x6, x0, 13
+	blt x6, x0, ad3
+	addi x6, x0, 18
+	blt x6, x0, ad4
+	addi x6, x0, 21
+	blt x6, x0, ad5
+	addi x6, x0, 24
+	blt x6, x0, ad8
+	addi x6, x0, 26
+	blt x6, x0, ad10
+	jal x0, print
 
-# incrementando pontuacao
-# exibir valor
+print:
+	addi x14, x14, 48
+	sb x14, 1024(x0)
+
+ad1:
+	addi x14, x14, 1
+	jalr x1, 0(x1)
+
+ad2:
+	addi x14, x14, 2
+	jalr x1, 0(x1)
+
+ad3:
+	addi x14, x14, 3
+	jalr x1, 0(x1)
+
+ad4:
+	addi x14, x14, 4
+	jalr x1, 0(x1)
+
+ad5:
+	addi x14, x14, 5
+	jalr x1, 0(x1)
+
+ad8:
+	addi x14, x14, 8
+	jalr x1, 0(x1)
+
+ad10:
+	addi x14, x14, 10
+	jalr x1, 0(x1)
+
+
+
+end: halt
+
+
+
+letras: .string "AEIOUDGTBCMNPFHVWYKRSJLXQZ"
+

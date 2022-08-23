4511667d917a43daf830198f5639a2028750e81c
Kaylane Lira <97316221+kaylanelira@users.noreply.github.com>
2022-08-22 22:08:03 -0300

    Update q7.asm

--- Lista1/q7.asm
+++ Lista1/q7.asm
@@ -1,11 +1,11 @@
 init:
 	lb x10, 0(x0) 
-	sb x10, 1030(x0) # potenciometro
-	addi x12, x0, 0x0e1 # maior que 10 
-	addi x15, x0, 0x133 # maior que 15
-	addi x16, x0, 0x19a # maior que 20
-	addi x17, x0, 0x200 # maior que 25
-	addi x18, x0, 0x266 # maior que 30
+	sb x10, 1030(x0) 	# potenciometro
+	addi x12, x0, 0x0e1	# maior que 10 
+	addi x15, x0, 0x133 	# maior que 15
+	addi x16, x0, 0x19a 	# maior que 20
+	addi x17, x0, 0x200 	# maior que 25
+	addi x18, x0, 0x266 	# maior que 30
 	
 loop:
 	lh x10, 1031(x0)
@@ -33,12 +33,14 @@ ledVdD_if:
 	blt x10, x18, ledVdD
 	jal x0, loop
 
+# liga led vermelho esquerdo
 ledVE:
 	addi x14, x0, 3	# LED 3
 	sb x14, 1033(x0)
 	addi x14, x0, 255
 	sb x14, 1034(x0)
-
+	
+	# desliga outros leds
 	jal x1, desligLED_AE
 	jal x1, desligLED_AD
 	jal x1, desligLED_VdE
@@ -46,13 +48,15 @@ ledVE:
 	jal x1, desligLED_VD
 
 	jal x0, loop
-
+	
+# liga led amarelo esquerdo
 ledAE:
 	addi x14, x0, 5
 	sb x14, 1033(x0)
 	addi x14, x0, 255
 	sb x14, 1034(x0)
 
+	# desliga outros leds
 	jal x1, desligLED_VE
 	jal x1, desligLED_AD
 	jal x1, desligLED_VdE
@@ -61,12 +65,14 @@ ledAE:
 
 	jal x0, loop
 
+# liga led amarelo direito
 ledAD:
 	addi x14, x0, 6
 	sb x14, 1033(x0)
 	addi x14, x0, 255
 	sb x14, 1034(x0)
 
+	# desliga outros leds
 	jal x1, desligLED_AE
 	jal x1, desligLED_VE
 	jal x1, desligLED_VdE
@@ -75,12 +81,14 @@ ledAD:
 
 	jal x0, loop
 
+# liga led verde esquerdo
 ledVdE:
 	addi x14, x0, 9 # led 9
 	sb x14, 1033(x0)
 	addi x14, x0, 255
 	sb x14, 1034(x0)
 
+	# desliga outros leds
 	jal x1, desligLED_AE
 	jal x1, desligLED_AD
 	jal x1, desligLED_VE
@@ -88,12 +96,14 @@ ledVdE:
 	jal x1, desligLED_VD
 	jal x0, loop
 
+# liga led verde direito
 ledVdD:
 	addi x14, x0, 10
 	sb x14, 1033(x0)
 	addi x14, x0, 255
 	sb x14, 1034(x0)
 	
+	# desliga outros leds
 	jal x1, desligLED_AE
 	jal x1, desligLED_AD
 	jal x1, desligLED_VdE
@@ -101,12 +111,14 @@ ledVdD:
 	jal x1, desligLED_VD
 	jal x0, loop
 
+# liga led vermelho esquerdo
 ledVD:
 	addi x14, x0, 11
 	sb x14, 1033(x0)
 	addi x14, x0, 255
 	sb x14, 1034(x0)
 
+	# desliga outros leds
 	jal x1, desligLED_AE
 	jal x1, desligLED_AD
 	jal x1, desligLED_VdE
@@ -162,7 +174,7 @@ desligLED_VD:
 	sb x14, 1034(x0)
 	jalr x0, 0(x1)
 
-# 11 -> 0011100001	0e1
+# 11 -> 0011100001 0e1
 # 15 -> 0100110010 132
 # 20 -> 0110011001 199
 # 25 -> 0111111111 lff

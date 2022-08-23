4b4739ee29d12b6b5c34cc6e7067db4620e3205f
Kaylane Lira <97316221+kaylanelira@users.noreply.github.com>
2022-08-22 21:41:53 -0300

    Update q6.asm

--- Lista1/q6.asm
+++ Lista1/q6.asm
@@ -1,81 +1,122 @@
-addi x18, x0, 13 # enter
-addi x9, x0, 1
-addi x16, x0, 10	# auxilia dezena
-addi x17, x0, 100	# auxilia centena
-# declarando fat 8! e 9!
-# 8! = 40320
-addi x19, x0, 40320
-# 9! = 362880
-addi x20, x0, 362880 
-# recebendo string do teclado (x11)
+addi x5, x0, 1
+addi x6, x0, 48
+addi x7, x0, 10		# auxilia dezena
+addi x8, x0, 100 	# auxilia centena
+addi x16, x0, 1000	# auxilia milhar
+lw x17, dezmil		# auxilia dezena de milhar
+lw x18, cemmil		# auxilia centena de milhar
+addi x9, x0, 13		# enter
+
+# leitura do teclado n em x11
 gets:
-	lb x11, 1025(x0)	# recebe char
-	addi x12, x11, -1 # x9 = num-1
-	beq x11, x18, enter	# verifica se foi enter/fim da string
+	lb x11, 1025(x0)
+	beq x11, x9, enter
+	sub x11, x11, x6	# transformando em int
+# se x11 == 1 -> 1!
+beq x11, x5, fat1
+# salvando n em x12 para fazer calculo da multiplicacao
+add x12, x0, x11
+# salvando n em x13
+add x13, x0, x11
+# x11 = n-1
+sub x11, x11, x5
 	
-	addi x28, x0, 8
-	beq x11, x28, soma8
-	addi x28, x0, 9
-	beq x11, x28,	soma9
+# Calculando fatorial
+fat:
+	beq x11, x5, endFat	# se chegar no 1, acaba fatorial
+	
+loopFat:
+	beq x12, x5, endLoopFat
+	add x14, x14, x13	# fazendo a multiplicacao por meio de soma
+	sub x12, x12, x5	# decrementa x12(n)
+	jal x0, loopFat
 
-	fat:
-		jal x1, mul
-		beq x12, x9, gets
-		addi x11, x11, -1
-		addi x12, x12, -1
-		beq x0, x0, fat
+endLoopFat:
+	add x13, x0, x14 	# coloca x14 em x13
+	sub x11, x11, x5 	# decrementa n lido pelo teclado para voltar para fatorial
+	add x12, x0, x11	# salvando n-1 em x12
+	jal x0, fat
 
-	jal x1, gets
+endFat:
+	add x21, x21, x13	# fazendo soma dos fatoriais lidos pelo teclado e salvando em x21
+	# limpando registradores para reutilizar
+	add x13, x0, x0
+	add x14, x0, x0
 
-# multiplicando x12 e x11
-# n = x11
-# n-1 = x12
-# const = x9
-# resultado = x10
+	jal x0, gets
 
-mul:
-	addi x6, x11, 0
-	addi x7, x12, 0
-	beq x0, x6, voltar
-	addi x6, x6, -1
-	add x10, x10, x7
-	beq x0, x0, mul
-voltar: jalr x0, 0(x1)
-soma8:
-	addi x10, x10, x19
-soma9:
-	addi x10, x10, x20
+# fazendo fatorial de 1
+fat1:
+	add x13, x0, x5
+	jal x0, endFat
 
-# printando centena
-dividC:
-	blt x10, x17, printC
+# Se for enter imprime o resultado
+enter:
+	jal x1, divid100000
+
+divid100000:
+	blt x21, x18, print100000
+	# cada vez que for possível retirar 100000 do num, adiciona 1 no reg (x22)
+	sub x21, x21, x18
+	add x22, x22, x5
+	jal x1, divid100000
+
+print100000:
+	add x22, x22, x6 	# convertendo para printar
+	sb x22, 1024(x0)	# # printa centena de milhar
+	jal x1, divid10000
+
+divid10000:
+	blt x21, x17, print10000
+	# cada vez que for possível retirar 10000 do num, adiciona 1 no reg (x22)
+	sub x21, x21, x17
+	add x23, x23, x5
+	jal x1, divid10000
+
+print10000:
+	add x23, x23, x6 	# convertendo para printar
+	sb x23, 1024(x0)	# printa dezena de milhar
+	jal x1, divid1000
+
+divid1000:
+	blt x21, x16, print1000
+	# cada vez que for possível retirar 1000 do num, adiciona 1 no reg (x22)
+	sub x21, x21, x16
+	add x24, x24, x5
+	jal x1, divid1000
+
+print1000:
+	add x24, x24, x6 	# convertendo para printar
+	sb x24, 1024(x0)	# printa milhar
+	jal x1, divid100
+
+divid100:
+	blt x21, x8, print100
 	# cada vez que for possível retirar 100 do num, adiciona 1 no reg que guarda as centenas (x20)
-	addi x10, x10, -100
-	addi x20, x20, 1
-	jal x1, dividC
+	sub x21, x21, x8
+	add x25, x25, x5
+	jal x1, divid100
 
-printC:
-	addi x20, x20, 48	# converte centena (x20) de acordo com tabela ASCII
-	sb x20, 1024(x0)	# printa centena
-	jal x1, dividDU		# segue para calcular dezena e unidade
+print100:
+	add x25, x25, x6	# converte centena (x20) de acordo com tabela ASCII
+	sb x25, 1024(x0)	# printa centena
+	jal x1, divid10		# segue para calcular dezena e unidade
 
 # printando dezena e unidade
-dividDU:
-	blt x10, x16, printDU
+divid10:
+	blt x21, x7, print10
 	# cada vez que for possível retirar 10 do num, adiciona 1 no reg que guarda as dezenas (x15)
-	addi x10, x10, -10
-	addi x15, x15, 1
-	jal x1, dividDU
+	sub x21, x21, x7
+	add x26, x26, x5
+	jal x1, divid10
 
-printDU: 
-	addi x15, x15, 48	# converte dezena (x15) de acordo com tabela ASCII
-	addi x10, x10, 48	# converte unidade (x14) de acordo com tabela ASCII
-	sb x15, 1024(x0)	# printa dezena
-	sb x10, 1024(x0)	# printa unidade
-	sb x18, 1024(x0)	# printa enter (para organizacao)
-	jal x0, enter
+print10: 
+	add x26, x26, x6	# converte dezena (x15) de acordo com tabela ASCII
+	add x21, x21, x6	# converte unidade (x14) de acordo com tabela ASCII
+	sb x26, 1024(x0)	# printa dezena
+	sb x21, 1024(x0)	# printa unidade
+	sb x9, 1024(x0)		# printa enter (para organizacao)
+halt
 
-enter:
-	jal x0, dividC
-	halt
-	# printando resultado
+dezmil: .word 10000
+cemmil: .word 100000
